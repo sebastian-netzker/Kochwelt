2b7c56886742fa18931af118b77232aba43763b4
nikmax <mail@antonsander.de>
2020-12-17 06:58:13 +0100

    final Release

--- js/recepy-list.js
+++ js/recepy-list.js
@@ -284,35 +284,18 @@ function createRecepyListInfo(
 
         <div class="div-number-ingridient">
 
-        <div class="div-number-ingridient-child">
+            <p class="p-number-ingridient1"> Zutaten für 
+             <button onclick="increaseNumber()" type="button" class="btn btn-recepylist btn-outline-success">+</button><p class="number_portion" id="number_portion"> ${number_portion}<p> <button onclick="decreaseNumber()" type="button" class="btn btn-recepylist btn-outline-success">-</button>  </p>
 
-            <p class="p-number-ingridient1"> Anzahl der Portionen: <p class="number_portion" id ="number_portion">  ${number_portion}</p> </p>
 
-            </div>
-
-            <div class="div-number-ingridient-child">
-
-            <button onclick="increaseNumber()" type="button" class="btn btn-recepylist btn-outline-success">+</button>
-
-             <button onclick="decreaseNumber()" type="button" class="btn btn-recepylist btn-outline-success">-</button> 
-
-             </div>
-
-            
-            
-          
+             
+          <p class="p-number-ingridient2">Personen</p>
 
         </div> 
 
        
 
         </div>`;
-
-       // <button onclick="increaseNumber()" type="button" class="btn btn-recepylist btn-outline-success">+<///button><p class="number_portion" id="number_portion"> ${number_portion}<p> <button onclick="decreaseNumber()//" type="button" class="btn btn-recepylist btn-outline-success">-</button> 
-
-
-             
-         // <p class="p-number-ingridient2">Personen</p>
 
  
 
